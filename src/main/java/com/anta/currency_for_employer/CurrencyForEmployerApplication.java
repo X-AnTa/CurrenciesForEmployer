81124320eb985ee0db4e4207d07e69cd40c867f3
X-AnTa <mrspbusdriver@gmail.com>
2022-03-31 16:40:08 +0600

    Initial commit

---
+++ src/main/java/com/anta/currency_for_employer/CurrencyForEmployerApplication.java
@@ -0,0 +1,13 @@
+package com.anta.currency_for_employer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyForEmployerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyForEmployerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anta/currency_for_employer/CurrencyForEmployerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anta.currency_for_employer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyForEmployerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
