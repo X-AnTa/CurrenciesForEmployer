392113206b3402e1f62f4e80119ba8b37ffa8d9c
X-AnTa <mrspbusdriver@gmail.com>
2022-04-01 09:52:53 +0600

    add methods saveUSer and findByLoginAndPassword

--- src/main/java/com/anta/currency_for_employer/service/UserServiceImpl.java
+++ src/main/java/com/anta/currency_for_employer/service/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.anta.currency_for_employer.service;
 
+import com.anta.currency_for_employer.entity.Role;
+import com.anta.currency_for_employer.entity.User;
 import com.anta.currency_for_employer.repository.RoleRepository;
 import com.anta.currency_for_employer.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
 
@@ -18,5 +20,26 @@ public class UserServiceImpl {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        Role userRole = roleRepository.findByName("ROLE_USER");
+
+        user.setRole(userRole);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findByLoginAndPassword(String login, String password) {
+        User user = userRepository.findByLogin(login);
+
+        if (user != null){
+            if (passwordEncoder.matches(password, user.getPassword())){
+                return user;
+            }
+        }
+        return null;
     }
 }
