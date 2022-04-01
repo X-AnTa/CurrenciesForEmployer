d67504af1c726f180abf52e5b6178a2200a773ff
X-AnTa <mrspbusdriver@gmail.com>
2022-04-01 09:33:37 +0600

    add repositories Role and User

---
+++ src/main/java/com/anta/currency_for_employer/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.anta.currency_for_employer.repository;
+
+import com.anta.currency_for_employer.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
---
+++ src/main/java/com/anta/currency_for_employer/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.anta.currency_for_employer.repository;
+
+import com.anta.currency_for_employer.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
