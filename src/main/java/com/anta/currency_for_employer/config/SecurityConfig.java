2b8e71cc31af2f88daa1e6cc964c58e96108f2a9
X-AnTa <mrspbusdriver@gmail.com>
2022-04-01 09:41:54 +0600

    add config

---
+++ src/main/java/com/anta/currency_for_employer/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.anta.currency_for_employer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
